b327ee29345cbef2fccd4cde1ad01065150810bc
YangSeongIn <didtjddls96@naver.com>
2022-09-17 19:08:26 +0900

    Update Lv3. 입국심사.cpp

--- Programmers/Lv3. 입국심사.cpp
+++ Programmers/Lv3. 입국심사.cpp
@@ -1,19 +1,31 @@
-def solution(n, times):
-    answer = 0
-    times.sort()
-    end = times[-1] * n
-    start = times[0]
-    while start <= end:
-        mid = (start + end) // 2
-        total = 0
-        for i in times:
-            total += mid // i
-            if total >= n:
-                break
+#include <string>
+#include <vector>
+#include <algorithm>
 
-        if total < n:
-            start = mid + 1
-        else:
-            answer = mid
-            end = mid - 1
-    return answer
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+    long long s = 1;
+    long long e = (long long)times[times.size() - 1] * n;
+    sort(times.begin(), times.end());
+    while (s <= e)
+    {
+        long long tmp = 0;
+        long long m = (s + e) / 2;
+        for (int i = 0; i < times.size(); i++)
+        {
+            tmp += m / times[i];
+        }
+        if (tmp >= n)
+        {
+            answer = m;
+            e = m - 1; 
+        }
+        else
+        {
+            s = m + 1;
+        }
+    }
+    return answer;
+}
