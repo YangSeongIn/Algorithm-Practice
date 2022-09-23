325004e1cd134e872bbf97fa98ae9c22cf337730
YangSeongIn <didtjddls96@naver.com>
2022-09-23 23:39:19 +0900

    Update Lv2. 두 큐 합 같게 만들기.cpp

--- Programmers/Lv2. 두 큐 합 같게 만들기.cpp
+++ Programmers/Lv2. 두 큐 합 같게 만들기.cpp
@@ -1,49 +1,41 @@
 #include <string>
 #include <vector>
-#include <iostream>
+#include <numeric>
+#include <deque>
 
 using namespace std;
 
 int solution(vector<int> queue1, vector<int> queue2) {
     int answer = 0;
-
-    long q1Sum = 0;
-    long q2Sum = 0;
-    int qSize = queue1.size();
-
-    for (int& i : queue1)
+    deque<int> dq1(queue1.begin(), queue1.end());
+    deque<int> dq2(queue2.begin(), queue2.end());
+    long a = accumulate(dq1.begin(), dq1.end(), 0);
+    long b = accumulate(dq2.begin(), dq2.end(), 0);
+    int maxSize = queue1.size();
+    while (a != b)
     {
-        q1Sum += i;
-    }
-    for (int& i : queue2)
-    {
-        q2Sum += i;
-    }
-    int pointer1{ 0 }, pointer2{ 0 };
-    while (answer < 2 * qSize + 2)
-    {
-        if (q1Sum == q2Sum)
-            return answer;
-        else if (q1Sum > q2Sum)
+        if (a > b)
         {
-            int tmp = queue1[pointer1];
-            q1Sum -= tmp;
-            pointer1++;
-
-            queue2.push_back(tmp);
-            q2Sum += tmp;
+            int dq1Front = dq1.front();
+            a -= dq1Front;
+            dq2.push_back(dq1Front);
+            b += dq1Front;
+            dq1.pop_front();
         }
-        else if (q1Sum < q2Sum)
+        else if (a < b)
         {
-            int tmp = queue2[pointer2];
-            q2Sum -= tmp;
-            pointer2++;
-
-            queue1.push_back(tmp);
-            q1Sum += tmp;
+            int dq2Front = dq2.front();
+            dq1.push_back(dq2Front);
+            b -= dq2Front;
+            dq2.pop_front();
+            a += dq2Front;
         }
         answer++;
+        if (answer > maxSize * 3) 
+        {
+            return -1;
+        }
     }
-
-    return -1;
+    
+    return answer;
 }
